ec9f4100334ccaa16c1f136543cab9b3726594fa
tarunkrishna11 <102866432+tarunkrishna11@users.noreply.github.com>
2026-02-20 12:02:46 -0700

    fixes of 2FA

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,10 @@ const dashboardRoutes = require('./dashboardRoutes');
 // const favouritesRoutes = require('./favouritesRoutes');
 const profileRoutes = require('./profileRoutes');
 // const physioRoutes = require('./physioRoutes');
+const userRoutes = require("./userRoutes");       // your big file
+// or split further: const authRoutes = require("./authRoutes");
 
+router.use("/", userRoutes);
 router.use('/', authRoutes);
 router.use('/dashboard', dashboardRoutes);
 // router.use('/exercises', exerciseRoutes);
