7c52850ab7bb5d0f0423a2e8e728face69cb1fad
vls24 <lalithashreya23@gmail.com>
2026-03-19 10:08:57 -0700

    Add detail route to workoutHistoryRoutes

--- routes/workoutHistoryRoutes.js
+++ routes/workoutHistoryRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { isAuthenticated } = require('../middleware/auth');
-const { getWorkoutHistory } = require('../controllers/workoutHistoryController');
+const { getWorkoutHistory, getWorkoutHistoryDetail } = require('../controllers/workoutHistoryController');
 
 router.get('/', isAuthenticated, getWorkoutHistory);
+router.get('/:id', isAuthenticated, getWorkoutHistoryDetail);
 
 module.exports = router;
